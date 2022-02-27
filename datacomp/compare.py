6ededcad7451ba2a873ac6fa97b3a13ea9093c14
Nathan <nathmclean@gmail.com>
2022-02-26 17:33:44 -0800

    WIP separate comparison result object/output formatting

---
+++ datacomp/compare.py
@@ -0,0 +1,144 @@
+import sys
+
+from dataclasses import dataclass
+from typing import Dict, List, Optional
+
+import pandas as pd
+
+
+@dataclass
+class ColumnResult:
+    dtype_match: bool = True
+    mismatch_percent: Optional[float] = None
+    mismatch_number: Optional[int] = None
+    mismatch_data: Optional[pd.DataFrame] = None
+
+
+@dataclass
+class CompareResult:
+    data_match: bool = True
+    left_only_columns: List[str] = []
+    right_only_columns: List[str] = []
+    index_match: bool = True
+    left_index_duplicates: Optional[pd.DataFrame] = None
+    right_index_duplicates: Optional[pd.DataFrame] = None
+    common_index_count: int = 0
+    left_index_count: int = 0
+    right_index_count: int = 0
+    left_only_indexes: Optional[pd.Series] = None
+    right_only_indexes: Optional[pd.Series] = None
+    column_results: Dict[str, ColumnResult] = {}
+
+
+def series_nonequal_index(ser1, ser2):
+    ser1_null = ser1.isnull()
+    ser2_null = ser2.isnull()
+    # Get index where series values are null in one series but the other
+    nonequal_null_idx = list(set(ser1[ser1_null].index) ^ set(ser2[ser2_null].index))
+    # Get index where series non-null values are nonequal
+    notnull_idx = set(ser1[~ser1_null].index) & set(ser2[~ser2_null].index)
+    nonequal_notnull_idx = ser1.loc[notnull_idx] != ser2.loc[notnull_idx]
+    nonequal_notnull_idx = ser1.loc[notnull_idx][nonequal_notnull_idx].index.tolist()
+    # Combine null/non-null indexes
+    return nonequal_null_idx + nonequal_notnull_idx
+
+
+def load_file(fpath):
+    if fpath.suffix == ".parquet":
+        return pd.read_parquet(fpath)
+    if fpath.suffix == ".csv":
+        return pd.read_csv(fpath)
+
+    raise RuntimeError(f"Unsupported file type {fpath.suffix} for {fpath}")
+
+
+def compare_data(data_fpath_1, data_fpath_2, index_cols):
+    result = CompareResult()
+
+    print(f"Loading {data_fpath_1} ...")
+    df1 = load_file(data_fpath_1)
+    print(f"Loading {data_fpath_2} ...")
+    df2 = load_file(data_fpath_2)
+
+    if set(df1.columns) != set(df2.columns):
+        result.data_match = False
+        result.left_only_columns = sorted(set(df1.columns) - set(df2.columns))
+        result.right_only_columns = sorted(set(df2.columns) - set(df1.columns))
+
+    if index_cols:
+        print("Indexing dataframes ...")
+        for df, data_fpath in [(df1, data_fpath_1), (df2, data_fpath_2)]:
+            try:
+                df.sort_values(index_cols, inplace=True)
+                df.set_index(index_cols, inplace=True)
+            except KeyError:
+                print()
+                print(
+                    f"⚠️  ERROR: index columns ({', '.join(index_cols)}) not found in {data_fpath}"
+                )
+                sys.exit(1)
+
+        print("Validating dataframe indices ...")
+        l_dup_indices = df1.index.duplicated()
+        r_dup_indices = df2.index.duplicated()
+        if l_dup_indices.any():
+            # Consider this an error because we are unable to determine if the data matches
+            result.data_match = False
+            result.left_index_duplicates = pd.DataFrame(
+                df.index[l_dup_indices].value_counts().rename("count")
+            )
+        if r_dup_indices.any():
+            result.data_match = False
+            result.right_index_duplicates = pd.DataFrame(
+                df.index[r_dup_indices].value_counts().rename("count")
+            )
+
+    print("Comparing dataframe indices ...")
+    result.left_index_count = len(df1)
+    result.right_index_count = len(df2)
+    if not df1.index.equals(df2.index):
+        result.data_match = False
+        result.index_match = False
+
+        result.left_only_indexes = pd.Series(
+            sorted(set(df1.index) - set(df2.index)), dtype=df1.index.dtype
+        )
+        result.right_only_indexes = pd.Series(
+            sorted(set(df2.index) - set(df1.index)), dtype=df2.index.dtype
+        )
+
+        common_ids = set(df1.index) & set(df2.index)
+        result.common_index_count = len(common_ids)
+
+        if not common_ids:
+            return result
+
+        df1 = df1.loc[common_ids]
+        df2 = df2.loc[common_ids]
+    else:
+        result.common_index_count = result.left_index_count
+
+    common_cols = [col for col in df1 if col in set(df2.columns)]
+    # quick check if dataframes match, otherwise show detailed differences
+    print("Comparing data ...")
+    if not df1[common_cols].equals(df2[common_cols]):
+        result.data_match = False
+
+        for idx, col in enumerate(common_cols):
+            print(f"Comparing column ({idx + 1}/{len(common_cols)}): {col} ...")
+            if not df1[col].equals(df2[col]):
+
+                mismatch_idx = series_nonequal_index(df1[col], df2[col])
+                mismatch_df = df1.loc[mismatch_idx, [col]].join(
+                    df2.loc[mismatch_idx, [col]], lsuffix="_LEFT", rsuffix="_RIGHT"
+                )
+                mismatch_pct = len(mismatch_idx) / len(df1) * 100
+
+                result.column_results[col] = ColumnResult(
+                    dtype_match=(df1[col].dtype == df2[col].dtype),
+                    mismatch_percent=mismatch_pct,
+                    mismatch_number=len(mismatch_idx),
+                    mismatch_data=mismatch_df,
+                )
+
+    return result
