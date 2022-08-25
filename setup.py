5045929ebaaa2335906d1d253d9883021f3435c8
kappamaki <nathmclean@gmail.com>
2022-08-24 17:09:49 -0700

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="datacomp",
-    version="0.5.0",
+    version="0.5.1",
     description="CLI for comparing tabular data files",
     author="kappamaki",
     install_requires=["pandas", "pyarrow"],
